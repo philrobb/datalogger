8eb5e1782fbd61a19d08abf1948dc790628a1b0f
phil.robb <phil.robb@maine.edu>
2019-05-02 08:05:00 -0400

    Fixed the problem with the data

--- sql24.php
+++ sql24.php
@@ -4,6 +4,7 @@ header('Content-Type: application/json');
 
 
 //Open  sensor_data.db
+//$db = new PDO('sqlite':/var/www/html/sensor_data.db);
 $db = new SQLite3('sensor_data.db', SQLITE3_OPEN_READONLY);
 
 if(!db){
@@ -11,7 +12,7 @@ if(!db){
 	exit();
 }
 
-$db_data = $db->query('SELECT DATE_TIME, TEMP1, IR, FULLY, VIS, LUX, TEMP2, PRESSURE, HUMID FROM sensor_data LIMIT 1440');
+$db_data = $db->query('SELECT DATE_TIME, TEMP1, IR, FULL, VIS, LUX, TEMP2, PRESSURE, HUMID FROM sensor_data LIMIT 1440');
 
 if(!data){
 	echo "Error accessing table\n";
@@ -23,44 +24,44 @@ $data_read = array();
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['TEMP1'];
-	$data_out['TEMP1'][] = $data_read;
+	$data_out['data1'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['IR'];
-	$data_out['IR'][] = $data_read; 
+	$data_out['data6'][] = $data_read; 
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['FULLS'];
-	$data_out['FULLY'][] = $data_read;
+	$data_out['data2'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['VIS'];
-	$data_out['VIS'][] = $data_read;
+	$data_out['data3'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['LUX'];
-	$data_out['LUX'][] = $data_read;
+	$data_out['data4'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['TEMP2'];
-	$data_out['TEMP2'][] = $data_read;
+	$data_out['data5'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['PRESSURE'];
-	$data_out['PRESSURE'][] = $data_read;
+	$data_out['data7'][] = $data_read;
 }
 while($row = $db_data->fetchArray(SQLITE3_ASSOC)){
 	$data_read['t'] = $row['DATE_TIME'];
 	$data_read['y'] = $row['HUMID'];
-	$data_out['HUMID'][] = $data_read;
+	$data_out['data8'][] = $data_read;
 }
 
-echo json_encode($data_out);
+echo json_encode($data_out,JSON_PRETTY_PRINT);
 
 ?>
